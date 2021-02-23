5b26628296fb762f4cea743ee794eda8561e85b9
Filip Martinsson <martinsson.filip@gmail.com>
2021-02-23 09:47:18 +0100

    tests done

--- test/dextest.js
+++ test/dextest.js
@@ -1,3 +1,64 @@
-//The user must have ETH deposited such that deposited eth >= buy order value
-//The user must have enough tokens deposited such that token balance > sell order amount
+
 //The first order ([0]) in the BUY order book should have the highest price
+
+const Dex = artifacts.require("Dex")
+const Link = artifacts.require("Link")
+const truffleAssert = require('truffle-assertions');
+
+contract("Dex", accounts => {
+    //The user must have ETH deposited such that deposited eth >= buy order value
+    it("should throw an error if ETH balance is too low when creating BUY limit order", async () => {
+        let dex = await Dex.deployed()
+        let link = await Link.deployed()
+        await truffleAssert.reverts(
+            dex.createLimitOrder(0, web3.utils.fromUtf8("LINK"), 10, 1)
+        )
+        dex.depositEth({value: 10})
+        await truffleAssert.passes(
+            dex.createLimitOrder(0, web3.utils.fromUtf8("LINK"), 10, 1)
+        )
+    })
+    //The user must have enough tokens deposited such that token balance >= sell order amount
+    it("should throw an error if token balance is too low when creating SELL limit order", async () => {
+        let dex = await Dex.deployed()
+        let link = await Link.deployed()
+        await truffleAssert.reverts(
+            dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 10, 1)
+        )
+        await link.approve(dex.address, 500);
+        await dex.deposit(10, web3.utils.fromUtf8("LINK"));
+        await truffleAssert.passes(
+            dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 10, 1)
+        )
+    })
+    //The first order ([0]) in the BUY order book should have the highest price
+    it("first order in the BUY order book should have the highest price", async () => {
+        let dex = await Dex.deployed()
+        let link = await Link.deployed()
+        await link.approve(dex.address, 500);
+        await dex.createLimitOrder(0, web3.utils.fromUtf8("LINK"), 1, 300)
+        await dex.createLimitOrder(0, web3.utils.fromUtf8("LINK"), 1, 100)
+        await dex.createLimitOrder(0, web3.utils.fromUtf8("LINK"), 1, 200)
+
+        let orderbook = await dex.getOrderBook(web3.utils.fromUtf8("LINK"), 0);
+        for (let i = 0; i < orderbook.length - 1; i++) {
+            const element = array[index];
+            assert(orderbook[i] >= orderbook[i+1])
+        }
+    })
+    //The first order ([0]) in the SELL order book should have the lowest price
+    it("first order in the SELL order book should have the lowest price", async () => {
+        let dex = await Dex.deployed()
+        let link = await Link.deployed()
+        await link.approve(dex.address, 500);
+        await dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 1, 300)
+        await dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 1, 100)
+        await dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 1, 200)
+
+        let orderbook = await dex.getOrderBook(web3.utils.fromUtf8("LINK"), 1);
+        for (let i = 0; i < orderbook.length - 1; i++) {
+            const element = array[index];
+            assert(orderbook[i] <= orderbook[i+1])
+        }
+    })
+})
